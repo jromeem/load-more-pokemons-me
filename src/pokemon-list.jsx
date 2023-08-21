e45478484db9ba41caa4916fe44e21e16a7154a4
Jerome Martinez <jromeem@gummie-bears.local>
2023-08-21 13:21:01 -0500

    adding initials for getting first five pokemon

--- src/pokemon-list.jsx
+++ src/pokemon-list.jsx
@@ -2,6 +2,27 @@
 // Make sure to replace limit and offset with the appropriate values
 // https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
 
-const PokemonList = () => {};
+import { useEffect, useState } from "react";
+
+const PokemonList = () => {
+    
+    const [initialPokemon, setInitialPokemon] = useState([]);
+    const getInitialPokemon = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=5&offset=0')
+                .then((response) => response.json())
+                .then((json) => {
+                    console.log(json)
+                    setInitialPokemon(json);
+                });
+    };
+
+    useEffect(() => {
+        getInitialPokemon();
+    }, [])
+
+    return <div>
+        <pre>{JSON.stringify(initialPokemon, null, 2)}</pre>
+    </div>
+};
 
 export default PokemonList;
