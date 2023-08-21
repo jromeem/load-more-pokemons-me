b7154faed6e1fea8e507fa13db9bb91c94a1737f
Jerome Martinez <jromeem@gummie-bears.local>
2023-08-21 15:12:59 -0500

    adding initials for last page test

--- src/pokemon-list.jsx
+++ src/pokemon-list.jsx
@@ -5,23 +5,62 @@
 import { useEffect, useState } from "react";
 
 const PokemonList = () => {
-    
-    const [initialPokemon, setInitialPokemon] = useState([]);
+
+    const [pokeCount, setPokeCount] = useState(0);
+    const [listedPokemon, setListedPokemon] = useState([]);
+    const [nextOffset, setNextOffset] = useState(0);
+    const [displayAmount, setDisplayAmount] = useState(5);
+    const [loadedAll, setLoadedAll] = useState(false);
+
+    const increaseOffset = () => setNextOffset(nextOffset + displayAmount);
+
     const getInitialPokemon = () => {
         fetch('https://pokeapi.co/api/v2/pokemon?limit=5&offset=0')
                 .then((response) => response.json())
                 .then((json) => {
-                    console.log(json)
-                    setInitialPokemon(json);
+                    console.log(json.results);
+                    setListedPokemon(json.results);
+                    setPokeCount(json.results.length);
+                    increaseOffset();
                 });
     };
+
+    const getNextPokemon = () => {
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=${displayAmount}&offset=${nextOffset}`)
+            .then((response) => response.json())
+            .then((json) => {
+                if (json.results.length < displayAmount) {
+                    setLoadedAll(true);
+                }
+                console.log(json.results);
+                setListedPokemon(listedPokemon.concat(json.results));
+                setPokeCount(pokeCount + json.results.length);
+                increaseOffset();
+            });
+    }
 
     useEffect(() => {
         getInitialPokemon();
     }, [])
 
     return <div>
-        <pre>{JSON.stringify(initialPokemon, null, 2)}</pre>
+        <div>
+            <p>Displaying {pokeCount} of 1281 results</p>
+            <input type="number" placeholder="display amount" value={displayAmount} onChange={(e) => setDisplayAmount(parseInt(e.target.value))}></input>
+            { !loadedAll ? 
+                <div>
+                    <button onClick={getNextPokemon}>Load more</button>
+                </div>
+                :
+                <></>
+            }
+            <ul>
+                {listedPokemon.map((poke, index) => {
+                    return <li key={index}>{poke.name}</li>
+                })}
+            </ul>
+
+        </div>
     </div>
 };
 
